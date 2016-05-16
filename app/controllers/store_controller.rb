ed475ab2ae1343b5fdd8e1907b8a7221ab420a53
oschebell <oschebell@gmail.com>
2016-05-16 16:56:26 +1000

    Add views for products index and show pages as well as home, about and contact pages

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,12 @@
 class StoreController < ApplicationController
   def home
+    @title = "Automated Store"
+    @product = Product.newest
+    @classic = Product.oldest
   end
 
   def about
+    @title = "About the Store"
   end
 
   def contact
